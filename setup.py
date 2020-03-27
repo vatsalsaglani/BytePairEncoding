a9836ed9d7cb431da971f7112ff8709ed310e7f1
vatsalsaglani <saglanivatsal@gmail.com>
2020-03-27 17:24:18 +0530

    Added TQDM requirement

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Build a corpus vocabulary using the Byte Pair Encoding(BPE) methodology and also tokenize sentences using the BPE tokenizer',
 	author = 'Vatsal Saglani',
     license = 'MIT',
-    # install_requires = ['tqdm'],
+    install_requires = ['tqdm'],
     packages = ['BPE'],
 	zip_safe = False
 
